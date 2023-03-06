8cc656b705e6613f32072c5ccc1f94931cccabad
slsecrets357 <xi.yang.li@mail.mcgill.ca>
2023-03-06 17:54:14 -0500

    led

--- input/src/encoder/encoderNODE.py
+++ input/src/encoder/encoderNODE.py
@@ -1,39 +1,14 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2019, Bosch Engineering Center Cluj and BFMC organizers
-# All rights reserved.
-
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-
-# 1. Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer.
-
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-
-# 3. Neither the name of the copyright holder nor the names of its
-#    contributors may be used to endorse or promote products derived from
-#    this software without specific prior written permission.
-
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE
-
 import sys
 sys.path.append('.')
 import os.path
 import time
 import math
 import serial
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
 
 import rospy
 
@@ -56,10 +31,41 @@ class encoderNODE():
         # encoder publisher object
         self.rate = rospy.Rate(30)
         self.encoder_publisher = rospy.Publisher("/automobile/encoder", encoder, queue_size=3)
+
+        self.fre = 10000
+        self.redval = 255
+        self.greval = 255
+        self.bluval = 255
+        self.brival = 255
+        #GPIO.setup(17,GPIO.OUT) #BLUE
+        #GPIO.setup(27,GPIO.OUT) #GREEN
+        #GPIO.setup(22,GPIO.OUT) #RED
+        #GPIO.setup(16,GPIO.OUT) #VCC
+        #self.vcc = GPIO.PWM(16,self.fre)
+        #self.red = GPIO.PWM(22,self.fre)
+        #self.gre = GPIO.PWM(27,self.fre)
+        #self.blu = GPIO.PWM(17,self.fre)
+        #self.vcc.start(self.brival/2.55)
+        #self.red.start(self.redval/2.55)
+        #self.blu.start(self.bluval/2.55)
+        #self.gre.start(self.greval/2.55)
+
+        def shutdown():
+            GPIO.cleanup()
+            # sys.exit()
+        rospy.on_shutdown(shutdown)
         
     #================================ RUN ========================================
     def run(self):
         rospy.loginfo("starting encoderNODE")
+        #i=15
+        #while i > 0:
+        #    i-=1
+        #    self.vcc.ChangeDutyCycle((self.brival)/2.55)
+        #    self.red.ChangeDutyCycle((255-self.redval)/2.55)
+        #    self.blu.ChangeDutyCycle((255-self.bluval)/2.55)
+        #    self.gre.ChangeDutyCycle((255-self.greval)/2.55)
+
         self._getting()
 
     #================================ GETTING ========================================
